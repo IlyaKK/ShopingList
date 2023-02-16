edfa808fce4f95c5551c82f0becd9df0271abe02
ilya <ilja.kasatckin@yandex.ru>
2023-02-17 01:42:51 +0300

    Add shop item fragment to main activity

--- app/src/main/java/com/ilya/shopinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/ilya/shopinglist/presentation/ShopItemActivity.kt
@@ -15,7 +15,9 @@ class ShopItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
         parseIntent()
-        launchRightMode()
+        if (savedInstanceState == null) {
+            launchRightMode()
+        }
     }
 
     private fun launchRightMode() {
@@ -25,7 +27,7 @@ class ShopItemActivity : AppCompatActivity() {
             else -> throw RuntimeException("Unknown screen mode $screenMode")
         }
         supportFragmentManager.beginTransaction()
-            .add(R.id.shop_item_container, fragment)
+            .replace(R.id.shop_item_container, fragment)
             .commit()
     }
 
